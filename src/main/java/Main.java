d2c8a6b0411962f31b6f83ec05bcb8438c8b99c0
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-02-06 11:55:24 -0600

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,16 @@ public class Main {
         // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
         while (true){
+
             System.out.print("$ ");
             String cmnd = scanner.nextLine();
+
+            if(cmnd.equals("exit")){
+                break;
+            }
+
             System.out.println(cmnd + ": command not found");
+
         }
 
     }
