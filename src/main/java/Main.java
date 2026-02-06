cc5bd75179258415e59c2b1d30925f90c683a93d
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-02-06 11:32:19 -0600

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+        Scanner scanner = new Scanner(System.in);
+        String cmbd = scanner.nextLine();
         System.out.print("$ ");
+        System.out.println(cmbd + " command not found");
     }
 }
